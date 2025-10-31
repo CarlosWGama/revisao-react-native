60485f8088e84c992fdf16b76322b9459d1dd322
CarlosWGama <carloswgama@gmail.com>
2025-10-31 18:19:44 -0300

    Aula - State e Props

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Form } from './src/components/form';
+//https://dontpad.com/carloswgama
+//npx expo start --tunnel
+//npm start
+//https://expo.dev/
 export default function App() {
+
+  const [ email, setEmail ] = useState<string>('');
+  const [ senha, setSenha ] = useState<string>('');
+  const [ error, setError ] = useState<false|string>(false);
+  // ------------------------------------------------
+  const handleLogin = () => {
+    if (email == 'teste@teste.com' && senha == '123456') {
+      setError(false);
+    } else {
+      setError('Email ou senha incorretos!');
+    }
+      
+  }
+  // ------------------------------------------------
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      
+     
+      <Form 
+        error={error}
+        setEmail={setEmail}
+        setSenha={setSenha}
+        handleLogin={handleLogin}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#0072be',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    padding: 30
   },
+  loginContainer: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 5
+  },
+  text: {
+    color: 'black'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    marginBottom: 10
+  }
 });
---
+++ README.md
@@ -0,0 +1,17 @@
+# Instalar
+
+Ao baixar rodar os comandos
+
+```sh
+git clone https://github.com/CarlosWGama/revisao-react-native.git
+cd revisao-react-native
+npm install
+npx expo start --tunnel
+```
+
+
+Sempre que atualizar o projeto rodar: 
+```sh
+npm install
+npx expo start --tunnel
+```
